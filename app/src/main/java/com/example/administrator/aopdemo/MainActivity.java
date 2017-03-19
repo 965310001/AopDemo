e5069bc16dac8c7b1fa8ee4fa64b4bcf40997673
965310001 <965310001@qq.com>
2017-03-19 16:34:41 +0800

    修改TAG

--- app/src/main/java/com/example/administrator/aopdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/aopdemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: ");
+        Log.d(TAG, "测试中的代码 ");
     }
 
     private static final String TAG = "MainActivity";
